de2cdfa927bc628a34f0f6edce1c862be0808029
AvivKermann <avivkermann@gmail.com>
2024-01-24 16:00:11 +0200

    finished all setups

--- main.go
+++ main.go
@@ -4,23 +4,42 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 const port = "8080"
+const filepathRoot = "."
+
+type apiConfig struct {
+	fileServerHits int
+}
 
 func main() {
-	app := http.NewServeMux()
-	corsMux := middlewareCors(app)
+	router := chi.NewRouter()
+	apiRouter := chi.NewRouter()
+	adminRouter := chi.NewRouter()
+	router.Mount("/api", apiRouter)
+	router.Mount("/admin", adminRouter)
 
-	myServer := &http.Server{
+	cfg := apiConfig{
+		fileServerHits: 0,
+	}
+
+	fsHandler := cfg.MiddlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	router.Handle("/app", fsHandler)
+	router.Handle("/app/*", fsHandler)
+	apiRouter.Get("/healthz", handlerHealthz)
+	adminRouter.Get("/metrics", cfg.handlerMetrics)
+	apiRouter.Get("/reset", cfg.handlerReset)
+
+	fmt.Printf("started local host on http://localhost:%s\n", port)
+	corsMux := middlewareCors(router)
+	server := &http.Server{
 		Handler: corsMux,
 		Addr:    ":" + port,
 	}
-
-	app.Handle("/", http.FileServer(http.Dir(".")))
-
-	fmt.Printf("started local host on http://localhost:%s\n", port)
-	err := myServer.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("server couldn't run")
 	}
